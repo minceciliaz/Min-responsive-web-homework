2e532f4adf125816c25e99860354ddfd721d58f8
minceciliaz <minzengzzz@gmail.com>
2021-07-08 02:02:38 -0700

    final

---
+++ js.js
@@ -0,0 +1,31 @@
+document.getElementById("001").addEventListener("click", function() {
+    alert("This is the homepage.");
+  });
+
+document.getElementById("002").addEventListener("click", function() {
+    alert("This is the homepage of Cecilia who wants to become a language engineer");
+  });
+
+document.getElementById("004").addEventListener("click", function() {
+    alert("Sorry you can't login at this moment");
+  });
+
+document.getElementById("003").addEventListener("click", function() {
+    alert("please send email to Cecilia to join the CC group manually.");
+  });
+
+
+document.getElementById("join").addEventListener("click", function() {
+    alert("The function is not available at this moment.");
+  });
+
+document.getElementById("button1").addEventListener("click", function() {
+    alert("The second webpage is in progress");
+  });
+
+
+document.getElementById("button2").addEventListener("click", function() {
+    alert("hello world!");
+  });
+
+
